6ad1a850cd6a8a4e7c50f5563b24215407db60d3
ViviFar <vivien.f@live.fr>
2021-01-29 17:53:36 +0100

    creation projet

---
+++ Assets/Scripts/GenericStateMachine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericStateMachine : MonoBehaviour
+{
+    private static GenericStateMachine instance;
+    public static GenericStateMachine Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GenericStateMachine>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.AddComponent<GenericStateMachine>();
+                    go.name = "StateMachine";
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
