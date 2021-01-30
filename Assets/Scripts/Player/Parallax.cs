c161f326e30ef1c11423e37d21c059f4b8fee8e5
Hyblis29 <54952850+Hyblis29@users.noreply.github.com>
2021-01-30 20:24:25 +0100

    Add ground tilemap and vertical parallax effect.

--- Assets/Scripts/Player/Parallax.cs
+++ Assets/Scripts/Player/Parallax.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
+    private float length, startposX, startposY;
     public GameObject cam;
     public float parallaxEffect;
 
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
+        startposX = transform.position.x;
+        startposY = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
@@ -19,17 +20,19 @@ public class Parallax : MonoBehaviour
     void Update()
     {
         float temp = cam.transform.position.x * (1 - parallaxEffect);
-        float dist = cam.transform.position.x * parallaxEffect;
+        float distX = cam.transform.position.x * parallaxEffect;
+        float distY = cam.transform.position.y * parallaxEffect;
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startposX + distX, startposY + distY, transform.position.z);
 
-        if (temp > startpos + length)
+        // si on dépasse les limites du background en X, on le décale
+        if (temp > startposX + length)
         {
-            startpos += length;
+            startposX += length;
         }
-        else if (temp < startpos - length)
+        else if (temp < startposX - length)
         {
-            startpos -= length;
+            startposX -= length;
         }
     }
 }
