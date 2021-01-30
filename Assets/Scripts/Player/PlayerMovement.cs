3701590d7a1c1fd5f8ea5efa3c6961c75969968a
ViviFar <vivien.f@live.fr>
2021-01-30 20:10:31 +0100

    Update - player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,13 @@ public class PlayerMovement : MonoBehaviour
     private float jumpForce = 600.0f;
 
     [SerializeField]
+    private float jumpForceFromConveyor = 300.0f; 
+
+    [SerializeField]
     private LayerMask whatIsGround;
+
+    [SerializeField]
+    private LayerMask EnvironmentLayer;
 
     [SerializeField]
     private Transform groundCheck;
@@ -25,15 +31,19 @@ public class PlayerMovement : MonoBehaviour
     private bool canJump = true;
 
     private bool facingRight = true;
+    private bool onConveyorBelt = false;
+
 
     private int playerLayer;
     private int platformLayer;
+
+    private BoxCollider2D col;
 
     // Start is called before the first frame update
     void Awake()
     {
         positionInitial = transform.position;
-
+        col = GetComponent<BoxCollider2D>();
         rg = GetComponentInChildren<Rigidbody2D>();
         if (rg == null)
         {
@@ -50,29 +60,48 @@ public class PlayerMovement : MonoBehaviour
         if (Time.timeScale == 0.0f)
             return;
 
-        MoveLeftRight();
+        MoveLeftRight(Time.deltaTime);
         vy = rg.velocity.y;
-        canJump = Physics2D.Linecast(transform.position, groundCheck.position, whatIsGround);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            canJump = Physics2D.Linecast(transform.position, groundCheck.position, whatIsGround);
+            Debug.Log("demande de saut");
             Jump();
         }
         rg.velocity = new Vector2(vx, vy);
         Physics2D.IgnoreLayerCollision(playerLayer, platformLayer, (vy > 0.0f));
     }
 
-    private void MoveLeftRight()
+    private void MoveLeftRight(float deltaTime)
     {
         vx = Input.GetAxisRaw("Horizontal") * speed + speedModifier;
+        Vector2 moveDir = new Vector2(vx * deltaTime, 0.2f);
+        Vector2 bottomRight = new Vector2(col.bounds.max.x, col.bounds.max.y);
+        Vector2 topLeft = new Vector2(col.bounds.min.x, col.bounds.min.y);
+        bottomRight += moveDir;
+        topLeft += moveDir;
+        if(Physics2D.OverlapArea(topLeft, bottomRight, EnvironmentLayer)){
+            vx = 0;
+        }
+        if (vx < 0 && transform.localScale.x>0)
+        {
+            transform.localScale = new Vector3(-1* transform.localScale.x, transform.localScale.y);
+        }
+        else if(vx> 0 && transform.localScale.x < 0)
+            transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y);
     }
 
     private void Jump()
     {
         if (canJump)
         {
+            Debug.Log("je peux sauter");
             vy = 0;
             canJump = false;
-            rg.AddForce(new Vector2(0, jumpForce));
+            if (onConveyorBelt)
+                rg.AddForce(new Vector2(jumpForceFromConveyor, jumpForce));
+            else
+                rg.AddForce(new Vector2(0, jumpForce));
         }
     }
 
@@ -81,6 +110,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "ConveyorBelt")
         {
+            onConveyorBelt = true;
             ConveyorBelt cb = collision.gameObject.GetComponent<ConveyorBelt>();
             speedModifier = cb.Speed;
             cb.PlayerIsOn = true;
@@ -91,6 +121,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "ConveyorBelt")
         {
+            onConveyorBelt = false;
             ConveyorBelt cb = collision.gameObject.GetComponent<ConveyorBelt>();
             speedModifier = 0;
             cb.PlayerIsOn = false;
@@ -101,4 +132,5 @@ public class PlayerMovement : MonoBehaviour
     {
         speedModifier = newSpeed;
     }
+    
 }
